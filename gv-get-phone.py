7c08b083a0face7334952311db6cf0396516b061
Nicholas Alipaz <nicholas@alipaz.net>
2016-11-29 14:28:41 -0800

    google voice

--- gv-get-phone.py
+++ gv-get-phone.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from googlevoice import Voice,Phone,util,Folder
+from gv-phone-dict import arg_to_phone
 import sys
 
 voice = Voice()
 voice.login()
 
-#util.pprint(voice.settings)
-#util.pprint(voice.contacts['phones'].values())
-
 execfile("/home/plex/.homeassistant/gv-phone-dict.py")
+
+filename = '/home/plex/.homeassistant/states/' + str(sys.argv[2])
 
 def returnValue():
     for phone in voice.phones:
--- gv-set-phone.py
+++ gv-set-phone.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from googlevoice import Voice,Phone,util
+from __future__ import print_function
 import sys
 
 voice = Voice()
@@ -7,10 +8,16 @@ voice.login()
 
 execfile("/home/plex/.homeassistant/gv-phone-dict.py")
 
+filename = '/home/plex/.homeassistant/states/' + str(sys.argv[2])
+
 for phone in voice.phones:
     if phone.phoneNumber == arg_to_phone(str(sys.argv[2])):
         if str(sys.argv[1]) == "enabled":
             phone.enable()
+            with open(filename, "w") as f:
+                f.write("enabled")
         elif str(sys.argv[1]) == "disabled":
             phone.disable()
+            with open(filename, "w") as f:
+                f.write("disabled")
 
