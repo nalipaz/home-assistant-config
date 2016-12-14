825185057d91e6e608dd7b6b9a153c80bc63ea7e
Nicholas Alipaz <nalipaz@labiomed.org>
2016-12-14 10:22:35 -0800

    Add screen locking/unlocking based on bt proximity

--- bt-proximity-mqtt.py
+++ bt-proximity-mqtt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import print_function
 import paho.mqtt.client as mqtt
-import sys,os,yaml,getpass,time
+import sys,os,yaml,getpass,time,subprocess
 
 config_file = open('/home/' + getpass.getuser() + '/.proximity', 'r')
 config = yaml.load(config_file)
@@ -14,11 +14,17 @@ client.tls_set(config['credentials']['certificate'])
 client.connect(config['credentials']['host'], int(config['credentials']['port']), 60)
 
 if len(sys.argv) > 1 and str(sys.argv[1]) == '-l':
+    # Lock the screen
+    os.system("xautolock -locknow")
+    # Send updated mqtt message about location to hass
     data = '{{"_type":"transition","wtst":{t},"tst":{t},"lat":{m[latitude]},"lon":{m[longitude]},"acc":2,"tid":"{c[tid]}","event":"leave","desc":"{m[zone]}","t":"c"}}'.format(c=config['credentials'], m=config['away_message'], t=timestamp)
     client.publish(topic, data)
     data = '{{"_type":"location","tid":"{c[tid]}","acc":27,"conn":"w","doze":false,"lat":{m[latitude]},"lon":{m[longitude]},"tst":{t},"event":"leave","desc":"{m[zone]}","t":"c"}}'.format(c=config['credentials'], m=config['away_message'], t=timestamp)
     client.publish(topic, data)
 else:
+    if str(sys.argv[1]) == '-u':
+        # Unlock the screen
+        os.system("pkill i3lock && xautolock -unlocknow")
     data = '{{"_type":"transition","wtst":{t},"tst":{t},"lat":{m[latitude]},"lon":{m[longitude]},"acc":2,"tid":"{c[tid]}","event":"enter","desc":"{m[zone]}","t":"c"}}'.format(c=config['credentials'], m=config['message'], t=timestamp)
     client.publish(topic, data)
     data = '{{"_type":"location","tid":"{c[tid]}","acc":27,"conn":"w","doze":false,"lat":{m[latitude]},"lon":{m[longitude]},"tst":{t},"event":"enter","desc":"{m[zone]}","t":"c"}}'.format(c=config['credentials'], m=config['message'], t=timestamp)
